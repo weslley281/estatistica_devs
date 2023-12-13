fb323bc70e9ed05ce2975d5625ba101633bbcd93
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-12-13 06:35:08 -0400

    a

--- estatisticas_dev.py
+++ estatisticas_dev.py
@@ -1,3 +1,9 @@
-import pandas as pandas
+import pandas as pd
 
-dict_medidas = {'idade': [15, 18, 25, 25, 40, 55, 58, 60, 80]}
+dict_medidas = {'idade': [15, 18, 25, 25, 40, 55, 58, 60, 80], 'altura': [160, 162, 165, 168, 172, 174, 174, 174, 176]}
+
+print(dict_medidas)
+
+df_medidas = pd.DataFrame.from_dict(dict_medidas)
+
+print(df_medidas)
